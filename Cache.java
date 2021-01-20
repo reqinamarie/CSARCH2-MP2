e2962aac5040cd195436880546404f2879c0077d
kenloquinte <kennethloquinte@gmail.com>
2021-01-20 22:56:30 +0800

    fixed some stuff

--- Cache.java
+++ Cache.java
@@ -1,67 +1,68 @@
 import java.util.*;
 
-public class Cache{
+public class Cache {
 
-    private ArrayList<String> contents = new ArrayList<String>();
+    private ArrayList<String> contents;
     private int size;
     private int block_size;
     private int num_blocks;
     private float access_time;
     private int mru;
 
-    public Cache(ArrayList<String> contents, int size, int block_size, int num_blocks, float access_time) {
-        this.contents = contents;
+    public Cache(int size, int block_size, int num_blocks, float access_time) {
+        this.contents = new ArrayList<String>(num_blocks);
         this.size = size;
         this.block_size = block_size;
         this.num_blocks = num_blocks;
         this.access_time = access_time;
-        this.mru = 0;	
-    } //constructor if cache memory size given is in blocks (size == num_blocks)
-    
-    public Cache(ArrayList<String> contents, int size, int block_size, float access_time){
-        this.contents = contents;
+        this.mru = 0;
+    } // constructor if cache memory size given is in blocks (size == num_blocks)
+
+    public Cache(int size, int block_size, float access_time) {
         this.size = size;
         this.block_size = block_size;
         this.num_blocks = size / block_size;
         this.access_time = access_time;
         this.mru = 0;
+        this.contents = new ArrayList<String>(this.num_blocks);
     }
-    
+
     public int getSize() {
         return size;
     }
-    
+
     public int getBlockSize() {
-        return block_size; 
+        return block_size;
     }
-    
-    public int getNumBlocks () {
+
+    public int getNumBlocks() {
         return num_blocks;
     }
-    
-    public float getAccessTime () {
-        return access_time; 
+
+    public float getAccessTime() {
+        return access_time;
     }
-    
-    public int getMRU () {
+
+    public int getMRU() {
         return mru;
     }
-    
-    public boolean isFull () {
+
+    public boolean isFull() {
         if (contents.size() == num_blocks)
             return true;
-        else return false;
+        else
+            return false;
     }
-    
-    public int find (int data) {
+
+    public int find(String data) {
         return contents.indexOf(data);
     }
-    
-    public void setMRU (int index) {
+
+    public void setMRU(int index) {
         this.mru = index;
     }
-    
-    public void setMRUBlock (String data) {
+
+    public void setMRUBlock(String data) {
         contents.set(mru, data);
     }
 }
--- Sequence.java
+++ Sequence.java
@@ -1,15 +1,19 @@
 import java.util.*;
 
-public class Sequence{
+public class Sequence {
     private ArrayList<String> data = new ArrayList<String>();
     private int loops;
 
-    public Sequence(ArrayList<String> data, int loops){
+    public Sequence(ArrayList<String> data, int loops) {
         this.data = data;
         this.loops = loops;
     }
-    
-    public ArrayList<String> getData(){
+
+    public ArrayList<String> getData() {
         return data;
+    }
+
+    public int getLoop() {
+        return loops;
     }
 }
